80a93bf9293bbd1d16d2e93a3734c5808569ea82
Eric Thelin <extern.eric.thelin@digg.se>
2025-10-10 11:56:23 +0200

    refactor: separate variable declaration and usage
    This avoids masking the command execution status code.
    
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- set-host.sh
+++ set-host.sh
@@ -2,7 +2,8 @@
 # Resolve the IP address of host.docker.internal and store it in HOST_IP
 
 export SET_HOST_EXTRA_PARAM='--add-host=host.docker.internal:host-gateway'
-export HOST_IP=$(docker run --rm $SET_HOST_EXTRA_PARAM alpine sh -c 'ping -c 1 host.docker.internal | grep PING | awk "{ print \$3 }" | tr -d "(): "')
+HOST_IP=$(docker run --rm $SET_HOST_EXTRA_PARAM alpine sh -c 'ping -c 1 host.docker.internal | grep PING | awk "{ print \$3 }" | tr -d "(): "')
+export HOST_IP
 
 # Output the HOST_IP for debugging (optional)
 echo "Resolved HOST_IP: $HOST_IP"
