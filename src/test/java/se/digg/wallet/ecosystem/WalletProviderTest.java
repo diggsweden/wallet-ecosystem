f74da50f96692f249a857f8ac1790532c3499974
Eric Thelin <extern.eric.thelin@digg.se>
2025-10-23 17:18:41 +0200

    test: check that we can create a WUA
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- src/test/java/se/digg/wallet/ecosystem/WalletProviderTest.java
+++ src/test/java/se/digg/wallet/ecosystem/WalletProviderTest.java
@@ -6,7 +6,13 @@ package se.digg.wallet.ecosystem;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
+import io.restassured.http.ContentType;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
 public class WalletProviderTest {
@@ -20,5 +26,24 @@ public class WalletProviderTest {
         .then()
         .assertThat().statusCode(200)
         .and().body("status", equalTo("UP"));
+  }
+
+  @Test
+  void createsWalletUnitAttestation() throws Exception {
+    given()
+        .relaxedHTTPSValidation()
+        .log().ifValidationFails()
+        .when()
+        .contentType(ContentType.JSON)
+        .body(String.format("""
+            { "walletId": "%s", "jwk": %s }""",
+            UUID.randomUUID(),
+            new ObjectMapper().writeValueAsString(
+                new ECKeyGenerator(Curve.P_256).generate().toPublicJWK().toJSONString())))
+        .post("https://wallet-provider.wallet.local/wallet-unit-attestation")
+        .then()
+        .log().ifValidationFails()
+        .assertThat().statusCode(200).and().body(matchesPattern(
+            "^[A-Za-z0-9]+\\.[A-Za-z0-9]+\\.[A-Za-z0-9\\-_]+$"));
   }
 }
