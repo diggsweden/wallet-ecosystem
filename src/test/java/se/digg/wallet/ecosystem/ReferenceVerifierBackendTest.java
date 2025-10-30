ff3698d5362e4a5573c08f09a638b69b1eec5b24
Eric Thelin <extern.eric.thelin@digg.se>
2025-10-30 11:26:12 +0100

    test: add simple test for reference verifier backend
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

---
+++ src/test/java/se/digg/wallet/ecosystem/ReferenceVerifierBackendTest.java
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2025 2025 The Wallet Ecosystem Authors
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.digg.wallet.ecosystem;
+
+import static org.hamcrest.CoreMatchers.is;
+import static se.digg.wallet.ecosystem.RestAssuredSugar.given;
+
+import org.junit.jupiter.api.Test;
+
+public class ReferenceVerifierBackendTest {
+
+  @Test
+  void isHealthy() {
+    given()
+        .when()
+        .get("https://refimpl-verifier-backend.wallet.local/actuator/health")
+        .then()
+        .assertThat().statusCode(200)
+        .and().body("status", is("UP"));
+  }
+}
