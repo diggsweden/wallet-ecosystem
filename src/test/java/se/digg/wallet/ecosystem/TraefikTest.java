ca4b3911001b9e74bac84df9717ca7f7305ec99e
Eric Thelin <extern.eric.thelin@digg.se>
2025-10-31 10:09:02 +0100

    test: add simple test for Traefik service
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

---
+++ src/test/java/se/digg/wallet/ecosystem/TraefikTest.java
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2025 2025 The Wallet Ecosystem Authors
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package se.digg.wallet.ecosystem;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static se.digg.wallet.ecosystem.RestAssuredSugar.given;
+
+import org.junit.jupiter.api.Test;
+
+public class TraefikTest {
+
+  @Test
+  void isHealthy() {
+    given()
+        .when()
+        .get("https://traefik.wallet.local")
+        .then()
+        .assertThat().statusCode(200)
+        .and().body("html.head.title", is("Traefik"))
+        .and().body(containsString("<div id=q-app>"));
+  }
+}
