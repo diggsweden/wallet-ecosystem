6edad4e1f4d50354e9a7cd9609e374087f2aab8d
Eric Thelin <extern.eric.thelin@digg.se>
2025-11-05 17:26:38 +0100

    fix: provide alternate URLs for PID issuer metadata
    This fixes the "Credential Issuer Metadata" and "SD-JWT VC Issuer
    Metadata" links displayed in the PID issuer UI.
    
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- src/test/java/se/digg/wallet/ecosystem/PidIssuerTest.java
+++ src/test/java/se/digg/wallet/ecosystem/PidIssuerTest.java
@@ -4,8 +4,11 @@
 
 package se.digg.wallet.ecosystem;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static se.digg.wallet.ecosystem.RestAssuredSugar.given;
 
@@ -14,6 +17,8 @@ import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PidIssuerTest {
   public static final String TOKEN_ENDPOINT =
@@ -21,18 +26,37 @@ public class PidIssuerTest {
   private static final String PID_ISSUER_NONCE_URL =
       "https://localhost/pid-issuer/wallet/nonceEndpoint";
 
-  @Test
-  void isHealthy() {
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "https://localhost/pid-issuer/.well-known/openid-credential-issuer",
+      "https://localhost/.well-known/openid-credential-issuer/pid-issuer"
+  })
+  void servesOpenIdCredentialIssuerMetadata(String url) {
     given()
         .when()
-        .get("https://localhost/pid-issuer/.well-known/openid-credential-issuer")
+        .get(url)
         .then()
-        .assertThat()
-        .statusCode(200)
-        .and()
-        .body(
+        .assertThat().statusCode(200)
+        .and().body(
+            "credential_issuer",
+            is("https://localhost/pid-issuer"))
+        .and().body(
+            "credential_request_encryption.jwks.keys",
+            not(empty()))
+        .and().body(
             "authorization_servers",
             hasItem("https://localhost/idp/realms/pid-issuer-realm"));
+  }
+
+  @Test
+  void servesJwtVcIssuerMetadata() {
+    given()
+        .when()
+        .get("https://localhost/.well-known/jwt-vc-issuer/pid-issuer")
+        .then()
+        .assertThat().statusCode(200)
+        .and().body("issuer", is("https://localhost/pid-issuer"))
+        .and().body("jwks.keys", not(empty()));
   }
 
   @Test
