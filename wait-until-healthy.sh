6733c1f454ed2d37e53e64932b3976011af03dc5
Erik Eriksson <erik.eriksson@digg.se>
2026-06-03 10:48:54 +0200

    fix: add kafka to wait script
    Signed-off-by: Erik Eriksson <erik.eriksson@digg.se>

--- wait-until-healthy.sh
+++ wait-until-healthy.sh
@@ -5,17 +5,23 @@
 # SPDX-License-Identifier: EUPL-1.2
 
 echo "Waiting for services to become healthy..."
-REGEX_FOR_ALL_SERVICES=$(yq -r '.services | to_entries | .[].key' docker-compose.yaml | sort | xargs | tr ' ' '|')
-REGEX_FOR_SERVICES_WITH_HEALTH_CHECKS='(keycloak|wallet-client-gateway|wallet-account|valkey|db) .*healthy'
-REGEX_FOR_INIT_CONTAINERS='keycloak-init Exited \(0\)'
-REGEX_FOR_OTHERS='(refimpl-verifier-backend|wallet-provider|pid-issuer|traefik|demo-verifier|trust-validator) Up'
+PROJECT_NAME=$(yq -r '.name' docker-compose.yaml)
+IGNORED_TEMPLATE_SERVICES=$(yq -r '.services | to_entries | .[].key | select(test("^x-"))' docker-compose.yaml | sort | xargs)
+if [ -n "$IGNORED_TEMPLATE_SERVICES" ]; then
+  echo "Ignoring compose template services: $IGNORED_TEMPLATE_SERVICES"
+fi
+REGEX_FOR_ALL_SERVICES=$(yq -r '.services | to_entries | .[].key | select(test("^x-") | not)' docker-compose.yaml | sort | xargs | tr ' ' '|')
+REGEX_FOR_CONTAINER_NAME="^($REGEX_FOR_ALL_SERVICES|$PROJECT_NAME-($REGEX_FOR_ALL_SERVICES)-[0-9]+) "
+REGEX_FOR_SERVICES_WITH_HEALTH_CHECKS='(keycloak|wallet-client-gateway|wallet-account|valkey|db|kafka-[123])(-[0-9]+)? .*healthy'
+REGEX_FOR_INIT_CONTAINERS='(keycloak-init|init-kafka)(-[0-9]+)? Exited \(0\)'
+REGEX_FOR_OTHERS='(refimpl-verifier-backend|wallet-provider|pid-issuer|traefik|demo-verifier|trust-validator|wallet-bff|hsm-worker|kafka-ui)(-[0-9]+)? Up'
 # Give services time to initialize (especially Keycloak)
 for i in {1..40}; do
   echo "Check attempt $i/40..."
   # Get statuses of all containers that have health checks
   UNHEALTHY=$(
     podman ps -a --format "{{.Names}} {{.Status}}" |
-      grep -E "$REGEX_FOR_ALL_SERVICES" |
+      grep -E "$REGEX_FOR_CONTAINER_NAME" |
       grep -v -E "($REGEX_FOR_SERVICES_WITH_HEALTH_CHECKS)|($REGEX_FOR_INIT_CONTAINERS)|($REGEX_FOR_OTHERS)" || true
   )
   if [ -z "$UNHEALTHY" ]; then
