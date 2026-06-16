cc6b1c48cd77effab77dff6cc44bcfd1885d4c41
Christoffer Killander <extern.christoffer.killander@digg.se>
2026-06-16 14:05:05 +0200

    Chore/bump hsm services (#200)
    * chore: bump hsm services
    
    Signed-off-by: Nicklas Jannesson <extern.nicklas.jannesson@digg.se>
    
    * fix(test): register wallet key before wallet unit attestation
    
    Signed-off-by: Christoffer Killander <extern.christoffer.killander@digg.se>
    
    ---------
    
    Signed-off-by: Nicklas Jannesson <extern.nicklas.jannesson@digg.se>
    Signed-off-by: Christoffer Killander <extern.christoffer.killander@digg.se>
    Co-authored-by: Nicklas Jannesson <extern.nicklas.jannesson@digg.se>

--- src/test/java/se/digg/wallet/ecosystem/PublicWalletClient.java
+++ src/test/java/se/digg/wallet/ecosystem/PublicWalletClient.java
@@ -4,11 +4,15 @@
 
 package se.digg.wallet.ecosystem;
 
+import com.nimbusds.jose.Algorithm;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.crypto.ECDSASigner;
+import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import java.util.Date;
@@ -17,28 +21,45 @@ public class PublicWalletClient implements WalletClient {
 
   private final WalletClientGatewayClient gateway = new WalletClientGatewayClient();
 
-  public String createWalletUnitAttestation(ECKey bindingKey, String nonce) throws JOSEException {
-    var accountId = gateway.createAccount(
-        "{\"deviceKey\": %s}".formatted(bindingKey.toPublicJWK().toJSONString()));
-    var sessionNonce = gateway.initChallenge(accountId, bindingKey.getKeyID());
-    var signedJwt = createSignedJwt(bindingKey, sessionNonce);
-    var session = gateway.respondToChallenge(signedJwt);
-    return gateway.tryCreateWalletUnitAttestation(session, nonce).then()
-        .assertThat().statusCode(201).extract()
-        .body().jsonPath().getString("jwt");
-  }
-
-  private static String createSignedJwt(ECKey ecJwk, String nonce)
-      throws JOSEException {
-    var claims = new JWTClaimsSet.Builder()
-        .claim("nonce", nonce)
-        .expirationTime(new Date(new Date().getTime() + 60 * 1000))
-        .build();
+  private static String createSignedJwt(ECKey ecJwk, String nonce) throws JOSEException {
+    var claims =
+        new JWTClaimsSet.Builder()
+            .claim("nonce", nonce)
+            .expirationTime(new Date(new Date().getTime() + 60 * 1000))
+            .build();
     var header = new JWSHeader.Builder(JWSAlgorithm.ES256).keyID(ecJwk.getKeyID()).build();
     var signedJwt = new SignedJWT(header, claims);
 
     signedJwt.sign(new ECDSASigner(ecJwk));
 
     return signedJwt.serialize();
+  }
+
+  public String createWalletUnitAttestation(ECKey bindingKey, String nonce) throws JOSEException {
+    ECKey deviceKey =
+        new ECKeyGenerator(Curve.P_256)
+            .keyID("device-key-123")
+            .algorithm(Algorithm.NONE)
+            .keyUse(KeyUse.SIGNATURE)
+            .generate();
+
+    var accountId =
+        gateway.createAccount(
+            "{\"deviceKey\": %s}".formatted(deviceKey.toPublicJWK().toJSONString()));
+    var sessionNonce = gateway.initChallenge(accountId, deviceKey.getKeyID());
+    var signedJwt = createSignedJwt(deviceKey, sessionNonce);
+    var session = gateway.respondToChallenge(signedJwt);
+
+    gateway.addWalletKey(session, bindingKey.toPublicJWK().toJSONString());
+
+    return gateway
+        .tryCreateWalletUnitAttestation(session, nonce)
+        .then()
+        .assertThat()
+        .statusCode(201)
+        .extract()
+        .body()
+        .jsonPath()
+        .getString("jwt");
   }
 }
