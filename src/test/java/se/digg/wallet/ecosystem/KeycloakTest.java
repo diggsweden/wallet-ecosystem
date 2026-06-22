20d520faa5dec5b771c415d532b3ececb26c3a83
Shirlene Lim <extern.shirlene.lim@digg.se>
2026-06-22 12:08:01 +0200

    fix: configure traefik to simulate istio keycloak whitelisting and add tests
    Signed-off-by: Shirlene Lim <extern.shirlene.lim@digg.se>

--- src/test/java/se/digg/wallet/ecosystem/KeycloakTest.java
+++ src/test/java/se/digg/wallet/ecosystem/KeycloakTest.java
@@ -4,8 +4,13 @@
 
 package se.digg.wallet.ecosystem;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static se.digg.wallet.ecosystem.RestAssuredSugar.given;
+import static se.digg.wallet.ecosystem.ServiceIdentifier.KEYCLOAK;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.Curve;
@@ -18,7 +23,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class KeycloakTest {
+class KeycloakTest {
 
   private final KeycloakClient keycloak = new KeycloakClient();
 
@@ -74,9 +79,52 @@ public class KeycloakTest {
     keycloak.tryGetOauthAuthorizationServerMetadata("pid-issuer-realm", strategy)
         .then()
         .assertThat().statusCode(200)
-        .and().body("issuer", equalTo(ServiceIdentifier.KEYCLOAK.getResourceRoot().resolve(
+        .and().body("issuer", equalTo(KEYCLOAK.getResourceRoot().resolve(
             "realms/pid-issuer-realm").toString()))
-        .and().body("token_endpoint", equalTo(ServiceIdentifier.KEYCLOAK.getResourceRoot().resolve(
+        .and().body("token_endpoint", equalTo(KEYCLOAK.getResourceRoot().resolve(
             "realms/pid-issuer-realm/protocol/openid-connect/token").toString()));
+  }
+
+  @Test
+  void pidIssuerRealmServesAccountConsole() {
+    keycloak.tryGetRealmAccount("pid-issuer-realm")
+        .then()
+        .assertThat().statusCode(200)
+        .and().contentType(containsString("text/html"));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "realms/pid-issuer-realm/protocol/openid-connect/3p-cookies/step1.html",
+      "realms/pid-issuer-realm/protocol/openid-connect/3p-cookies/step2.html"
+  })
+  void pidIssuerRealmLoadsResourcesExternally(String path) {
+    given().when()
+        .get(KEYCLOAK.getResourceRoot().resolve(path))
+        .then().assertThat().statusCode(200)
+        .and().contentType(containsString("text/html"));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "auth",
+      "logout",
+      "introspect",
+      "userinfo",
+      "revoke",
+      "certs"
+  })
+  void masterRealmDeniesRiskyProtocolEndpointsExternally(String path) {
+    given()
+        .when().get(KEYCLOAK.getResourceRoot().resolve("realms/master/protocol/" + path))
+        .then().assertThat().statusCode(anyOf(is(403), is(404)));
+  }
+
+  @ParameterizedTest
+   @ValueSource(strings = {"realms/master/console/", "realms/admin/master/console"})
+  void masterRealmAndAdminConsoleIsBlockedExternally() {
+    given()
+        .when().get(KEYCLOAK.getResourceRoot().resolve())
+        .then().assertThat().statusCode(anyOf(is(403), is(404)));
   }
 }
