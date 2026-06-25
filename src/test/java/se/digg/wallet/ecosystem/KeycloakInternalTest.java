348fa98e0cb87a9aca794e29d7ff2fdb927ccfb7
Eric Thelin <extern.eric.thelin@digg.se>
2026-06-25 18:07:16 +0200

    test: check that admin console points to internal URLs
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

--- src/test/java/se/digg/wallet/ecosystem/KeycloakInternalTest.java
+++ src/test/java/se/digg/wallet/ecosystem/KeycloakInternalTest.java
@@ -7,10 +7,20 @@ package se.digg.wallet.ecosystem;
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static se.digg.wallet.ecosystem.ServiceIdentifier.KEYCLOAK_INTERNAL;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisabledIfEnvironmentVariable(
     named = "DIGG_WALLET_ECOSYSTEM_SKIP_TESTS_FOR_KEYCLOAK_INTERNAL",
@@ -44,5 +54,25 @@ class KeycloakInternalTest {
         .and().body(containsString("<title>Keycloak Administration Console</title>"))
         .and().body(containsString("id=\"app\""))
         .and().body(containsString("rel=\"icon\""));
+  }
+
+  @ParameterizedTest
+  @MethodSource("masterAdminConsoleUrls")
+  void adminConsolePointsToInternalUrls(String url) {
+    assertThat(url, is(KEYCLOAK_INTERNAL.toString()));
+  }
+
+  private static Stream<Arguments.ArgumentSet> masterAdminConsoleUrls()
+      throws JsonProcessingException {
+    String environment = clientFor(KEYCLOAK_INTERNAL).tryGetMasterAdminConsole()
+        .then().assertThat().statusCode(200)
+        .extract().body().htmlPath().get("html.body.script.find { it.@id == 'environment' }");
+
+    JsonNode root = new ObjectMapper().readTree(environment);
+    assertThat(root.getNodeType(), is(JsonNodeType.OBJECT));
+    return root.propertyStream()
+        .map(entry -> Map.entry(entry.getKey(), entry.getValue().asText()))
+        .filter(entry -> entry.getValue().startsWith("http"))
+        .map(entry -> Arguments.argumentSet(entry.getKey(), entry.getValue()));
   }
 }
